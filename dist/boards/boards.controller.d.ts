60b4d1530243bc406cf891e23c8ccc3aee96209b
ByeolNabi <20183338@office.deu.ac.kr>
2024-08-22 11:55:52 +0900

    improve deleteBoard service

--- dist/boards/boards.controller.d.ts
+++ dist/boards/boards.controller.d.ts
@@ -1,8 +1,12 @@
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 export declare class BoardsController {
     private boardsService;
     constructor(boardsService: BoardsService);
     getAllBoard(): Board[];
-    createBoard(title: string, description: string): Board;
+    createBoard(createBoardDto: CreateBoardDto): Board;
+    getBoardById(id: string): Board;
+    deleteBoard(id: string): void;
+    updateBoardStatus(id: string, status: BoardStatus): Board;
 }
--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -6,6 +6,8 @@ import {
   Param,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
@@ -21,6 +23,7 @@ export class BoardsController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe) // 핸들러 레벨에서 만들어 준 것
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
   }
