fdf4bc14d67c2810be7c52f9248952345400f06e
ByeolNabi <20183338@office.deu.ac.kr>
2024-09-03 23:43:57 +0900

    signUp에서 unique를 판단, 에러 구현

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,10 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
@@ -13,6 +17,14 @@ export class UserRepository extends Repository<User> {
     const { username, password } = authCredentialDto;
     const user = this.create({ username, password });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username Already Existing');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
