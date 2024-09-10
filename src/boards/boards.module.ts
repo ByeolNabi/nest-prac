35dd22b26741370a32076913fe99f795f2e99c97
ByeolNabi <20183338@office.deu.ac.kr>
2024-09-10 14:13:29 +0900

    entity간 관계 설정, controller레벨에서 인증 관리

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,12 +4,11 @@ import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
 import { Board } from './board.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports:[
-    TypeOrmModule.forFeature([Board]),
-  ],
+  imports: [TypeOrmModule.forFeature([Board]), AuthModule],
   controllers: [BoardsController],
-  providers: [BoardsService, BoardRepository]
+  providers: [BoardsService, BoardRepository],
 })
 export class BoardsModule {}
